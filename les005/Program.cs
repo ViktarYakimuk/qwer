8321a5208216fb98610e540972ae3fe2e6a64ac7
ViktarYakimuk <ViktarYakimuk>
2022-09-01 22:57:25 +0300

    приветствие.

---
+++ les005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Витя")
+{
+   Console.WriteLine("Привет Витенька. ");
+}
+
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username);
+}
